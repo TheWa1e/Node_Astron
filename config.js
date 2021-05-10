6273eccd59767f87390dd3bbb8c1ed917597c194
TheWa1e <58726425+TheWa1e@users.noreply.github.com>
2021-05-10 11:17:48 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,15 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-	host     : 'us-cdbr-east-03.cleardb.com',
-	user     : 'bab2685f19d08c',
-	password : '415d70d5',
-	database : 'heroku_e0122e6c6548d22'
+  host     : 'us-cdbr-east-03.cleardb.com',
+  user     : 'bab2685f19d08c',
+  password : '415d70d5',
+  database : 'heroku_e0122e6c6548d22'
 });
-
-function connect(callback=null){
-    console.log("MYSQL TAKE CONNECT");
-    mysql=mysqldb.createConnection({host: "localhost", user: "*", password: "*", database:"*"});
-    mysql.on('error', function(err) {
-      console.log("---" +err.message);
-      console.log("---" +err.code);
-    });
-  if(callback)setTimeout(callback,100);
-}
-connect();
-
+connection.connect(function(err){
+  if(!err){
+    console.log("Database is connected");
+  }else{
+    console.log("Error while connecting with database");
+  }
+});
 module.exports = connection; 
