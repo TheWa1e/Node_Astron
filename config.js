168d0956afdad7ef99367142b86b1466dbf9ccac
TheWa1e <58726425+TheWa1e@users.noreply.github.com>
2021-05-10 09:56:57 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,12 +5,18 @@ var pool = mysql.createPool({
 	password : '415d70d5',
 	database : 'heroku_e0122e6c6548d22',
 });
-pool.connect(function(err){
-	if(!err){
-		console.log("Database is connected");
-	}else{
-		console.log("Error while connecting with database");
-	}
+pool.getConnection(function(err, connection) {
+  if(err) {
+    console.log(err);
+  }
+  let sql = "SELECT * FROM `message";
+  connection.query(sql, [], function(err, results) {
+    connection.release(); // always put connection back in pool after last query
+    console.log(results);
+    if(err) {
+      console.log(err);
+    }
+  });
 });
 module.exports = pool;
 pool.query("SET SESSION wait_timeout = 604800");
