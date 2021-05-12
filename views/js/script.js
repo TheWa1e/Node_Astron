858a9f5986f57a8121371b7e55300e71e1983613
TheWa1e <58726425+TheWa1e@users.noreply.github.com>
2021-05-13 00:47:17 +0300

    Update script.js

--- views/js/script.js
+++ views/js/script.js
@@ -66,3 +66,16 @@ var app = new Vue ({
     show: false
   }
 })
+
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
