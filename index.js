dc5417f28e8b3f6682810125090d8dc30e5db5b2
TheWa1e <58726425+TheWa1e@users.noreply.github.com>
2021-05-13 15:48:19 +0300

    commit

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 var express=require("express");
 var bodyParser=require('body-parser');
 const { join } = require("path");
- 
-var pool = require('./controllers/config');
+var connection = require('./controllers/config');
 var app = express();
- 
-/*var authenticateController=require('./controllers/authenticate-controller');
-var registerController=require('./controllers/register-controller');
-var sendpasswordController=require('./controllers/send-password-controller');*/
 var sendmessageController=require('./controllers/send-message-controller');
 
 app.use(bodyParser.urlencoded({extended:true}));
@@ -21,21 +16,14 @@ app.get("/auth_config.json", (req, res) => {
   res.sendFile(join(__dirname, "auth_config.json"));
 });
 
-pool.query('SELECT message FROM message', function(error, result, fields){
+connection.query('SELECT message FROM message', function(error, result, fields){
     app.get("/", function (req, res) {
       res.render("index.ejs", { params: result });
     });
 });
 
-/*app.post('/api/register',registerController.register);
-app.post('/api/authenticate',authenticateController.authenticate);
-app.post('/controllers/send-password-controller',sendpasswordController.sendpassword);*/
 app.post('/controllers/send-message-controller',sendmessageController.sendmessage);
-
-console.log(/*authenticateController*/);
-/*app.post('/controllers/register-controller', registerController.register);
-app.post('/controllers/authenticate-controller',authenticateController.authenticate);
-app.post('/controllers/send-password-controller',sendpasswordController.sendpassword);*/
+console.log(sendmessageController);
 app.post('/controllers/send-message-controller',sendmessageController.sendmessage);
 
 var request = require("request");
