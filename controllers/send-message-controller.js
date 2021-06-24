98eb0f307497ab8e756ea5bddc5b94cd01119530
Redhead <stadnikov.pasha@mail.ru>
2021-06-25 00:52:08 +0300

    Update send-message-controller.js

--- controllers/send-message-controller.js
+++ controllers/send-message-controller.js
@@ -1,5 +1,13 @@
 var express=require("express");
-var connection = require('./config');
+var mysql      = require('mysql');
+
+var db_config = {
+	host     : 'us-cdbr-east-03.cleardb.com',
+	user     : 'bab2685f19d08c',
+	password : '415d70d5',
+	database : 'heroku_e0122e6c6548d22',
+	port: 3306,
+};
 
 module.exports.sendmessage=function(req,res){
   var message={
@@ -8,8 +16,18 @@ module.exports.sendmessage=function(req,res){
     "phone":req.body.phone,
     "message":req.body.message
   }
+  let connection = mysql.createConnection(db_config);
+  
+  connection.connect(function(err) {              
+    if(err) {                                    
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 0); 
+    }                                     
+  });   
+  
   connection.query('INSERT INTO message SET ?',message, function (error, results, fields) {
     if(error) return console.log(error);
     res.redirect("./../");
   });
+  connection.end();
 }
