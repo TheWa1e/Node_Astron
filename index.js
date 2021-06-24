637b5070c9f777f6d91de0c0d3006f341169edfe
Redhead <stadnikov.pasha@mail.ru>
2021-06-25 00:53:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var express=require("express");
 var bodyParser=require('body-parser');
 const { join } = require("path");
-var connection = require('./controllers/config');
 var app = express();
 var sendmessageController=require('./controllers/send-message-controller');
 
@@ -15,13 +14,27 @@ app.use(express.static(join(__dirname, "views")));
 app.get("/auth_config.json", (req, res) => {
   res.sendFile(join(__dirname, "auth_config.json"));
 });
-
+var message={
+    "name":req.body.username,
+    "email":req.body.emailaddres,
+    "phone":req.body.phone,
+    "message":req.body.message
+  }
+let connection = mysql.createConnection(db_config);
+  
+connection.connect(function(err) {              
+  if(err) {                                    
+    console.log('error when connecting to db:', err);
+    setTimeout(handleDisconnect, 0); 
+  }                                     
+});   
 connection.query('SELECT message FROM message', function(error, result, fields){
     app.get("/", function (req, res) {
       res.render("index.ejs", { params: result });
       console.log(result);
     });
 });
+connection.end();
 
 app.post('/controllers/send-message-controller',sendmessageController.sendmessage);
 console.log(sendmessageController);
